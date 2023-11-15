3e2d2e9f9d93b8b472d07c5dcd4eb496b9a1228e
junhong <junhong@solderfieldgroup.com>
2023-11-15 16:10:31 +0800

    light dark prototype

--- web-portfolio/src/topbar.js
+++ web-portfolio/src/topbar.js
@@ -1,13 +1,30 @@
 import './App.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function TopBar() {
+  const [darkMode, setdarkMode] = useState(false);
+  useEffect(() => {
+    if(darkMode){
+      document.querySelector(':root').style.setProperty('--background', 'grey');
+    }else{
+      document.querySelector(':root').style.setProperty('--background', 'white');
+    }
+   }, [darkMode]);
+
   return (
     <div className="TopBar">
         <div className="MenuItems">
             <Link className="MenuItem" to="/">Home</Link>
             <Link className="MenuItem" to="/projects">Projects</Link>
             <Link className="MenuItem" to="/updates">Updates</Link>
+            <label className="switch">
+              <input 
+              type="checkbox"
+              checked={darkMode} 
+              onChange={(e) => setdarkMode(e.target.checked)}/>
+              <span className="slider round"></span>
+            </label>
         </div>
     </div>
   );
