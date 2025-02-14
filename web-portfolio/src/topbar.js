fe362aca0d09863c0591589eaceae47af3ded0d1
moroha29 <84175294+moroha29@users.noreply.github.com>
2025-02-14 09:19:07 +0800

    update

--- web-portfolio/src/topbar.js
+++ web-portfolio/src/topbar.js
@@ -4,14 +4,9 @@ import { useEffect, useState } from 'react';
 
 export default function TopBar() {
   const [darkMode, setdarkMode] = useState(false);
-  // const [collapsed, setCollapsed] = useState(false);
-
-  // const handleToggleSidebar = () => {
-  //   setCollapsed(!collapsed);
-  // };
   useEffect(() => {
     if(darkMode){
-      document.querySelector(':root').style.setProperty('--background', 'grey');
+      document.querySelector(':root').style.setProperty('--background', '#36393e');
       document.querySelector(':root').style.setProperty('--color-text', 'white');
     }else{
       document.querySelector(':root').style.setProperty('--background', 'white');
@@ -21,10 +16,9 @@ export default function TopBar() {
 
   return (
     <div className="TopBar">
-        {/* <button className="projectToggle" onClick={handleToggleSidebar}>Toggle Sidebar</button> */}
         <div className="MenuItems">
             <Link className="MenuItem" to="/">Home</Link>
-            <Link className="MenuItem" to="/projects">Projects</Link>
+            {/* <Link className="MenuItem" to="/projects">Projects</Link> */}
           <div className="MenuItem SwitchItem">
             <label className='switch'>
               <input
